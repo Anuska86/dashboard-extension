629fb88c3caefb0248d55783405867fe11a5286c
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-14 16:00:47 +0100

    autor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const autorContainer = document.getElementById("autor-container");
+
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature,mountains,ocean"
 )
@@ -5,7 +7,11 @@ fetch(
   .then((data) => {
     console.log(data);
 
+    //Background image
     document.body.style.backgroundImage = `url(${data.urls.full})`;
+
+    //Autor's name
+    autorContainer.textContent = `Autor: ${data.user.name}`;
   })
   .catch((error) => {
     console.error("Something went wrong: ", error);
