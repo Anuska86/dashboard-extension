033d52d35c95b38e0c9fc2f7b699f01a6916b6ba
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-14 17:29:05 +0100

    news

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const autorContainer = document.getElementById("author-container");
+const authorContainer = document.getElementById("author-container");
+const newsUrl = `https://newsapi.org/v2/top-headlines?language=en&pageSize=1&apiKey=${newsKey}`;
 
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature,mountains,ocean"
@@ -11,8 +12,25 @@ fetch(
     document.body.style.backgroundImage = `url(${data.urls.full})`;
 
     //Autor's name
-    autorContainer.textContent = `By: ${data.user.name}`;
+    authorContainer.textContent = `Picture by: ${data.user.name}`;
   })
   .catch((error) => {
     console.error("Something went wrong: ", error);
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp5252093.jpg)`;
+    authorContainer.textContent = `Picture by caverman`;
   });
+
+//News
+
+fetch(newsUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.articles && data.articles.length > 0) {
+      const article = data.articles[0];
+
+      const newsEl = document.querySelector(".top-page p:first-child");
+
+      newsEl.textContent = `Breaking: ${article.title}`;
+    }
+  })
+  .catch((err) => console.error("News error:", err));
