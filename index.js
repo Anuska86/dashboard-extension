786bfab44aea21e40b082f4db4c2bd29325865ca
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-14 12:17:06 +0100

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature,mountains,ocean"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+  })
+  .catch((error) => {
+    console.error("Something went wrong: ", error);
+  });
